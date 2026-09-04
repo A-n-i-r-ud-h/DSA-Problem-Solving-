3cca562ce32101a6d149cc547c92b6a8325e143f
A-n-i-r-ud-h <158015870+A-n-i-r-ud-h@users.noreply.github.com>
2026-09-04 07:11:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotateArr(int arr[], int d) {
+        int n=arr.length;
+        d=d%n;
+        if(d==0 || n==0) return;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+        
+        public void reverse(int arr[],int left,int right){
+            while(left<=right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+            }
+            
+        }
+        
+    
+}
